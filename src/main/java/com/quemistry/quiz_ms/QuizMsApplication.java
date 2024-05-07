22070d87eea3957fd72c690a342519aab8be7a30
Htet Thura Zaw (Raymond) <htetthurazaw@gmail.com>
2024-05-07 20:16:41 +0800

    project skeleton structure

---
+++ src/main/java/com/quemistry/quiz_ms/QuizMsApplication.java
@@ -0,0 +1,13 @@
+package com.quemistry.quiz_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quemistry/quiz_ms/QuizMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quemistry.quiz_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
