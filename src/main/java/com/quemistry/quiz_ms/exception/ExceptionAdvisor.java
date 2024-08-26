3c056084204eb0bee7462663b73ff7ef4cc6537b
Wang Shiji <scglwsj@163.com>
2024-08-26 23:55:11 +0800

    add a log for internal error part for debugging

--- src/main/java/com/quemistry/quiz_ms/exception/ExceptionAdvisor.java
+++ src/main/java/com/quemistry/quiz_ms/exception/ExceptionAdvisor.java
@@ -2,6 +2,8 @@ package com.quemistry.quiz_ms.exception;
 
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,6 +12,8 @@ import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class ExceptionAdvisor {
+  private static final Logger log = LoggerFactory.getLogger(ExceptionAdvisor.class);
+
   @ExceptionHandler(ApplicationException.class)
   public ResponseEntity<ExceptionResponse> handleApplicationException(
       ApplicationException ex, WebRequest request) {
@@ -20,6 +24,7 @@ public class ExceptionAdvisor {
   @ResponseStatus(INTERNAL_SERVER_ERROR)
   @ExceptionHandler(Exception.class)
   public ExceptionResponse handleGlobalException(Exception ex, WebRequest request) {
+    log.error("Internal server error", ex);
     return new ExceptionResponse(ex.getMessage());
   }
 }
